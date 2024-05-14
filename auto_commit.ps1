7d96148526d2d81c080986df9056bcedea653799
Wojtek <w.kuczkowski@mentzen.pl>
2024-05-14 11:42:04 +0200

    Automatyczny commit

--- auto_commit.ps1
+++ auto_commit.ps1
@@ -1,9 +1,7 @@
 # auto_commit.ps1
 
-# Znajdź ścieżkę obecnego katalogu
+# Ustawienia
 $currentDirectory = (Get-Location).Path
-
-# Przejdź do katalogu projektu
 Set-Location -Path $currentDirectory
 
 # Opis commita jako pierwszy argument
@@ -11,8 +9,22 @@ param (
     [string]$commitMessage = "Automatyczny commit"
 )
 
+# Upewnij się, że opis commita jest podany
+if (-not $commitMessage) {
+    Write-Output "Użycie: .\auto_commit.ps1 <opis commita>"
+    exit 1
+}
+
 # Wykonaj komendy git
 git add .
 git commit -m $commitMessage
-git pull https://github.com/Hicior/Calendesk.git main
+
+# Synchronizuj zmiany ze zdalnym repozytorium
+git pull origin main --no-edit  # Opcja --no-edit pomija otwieranie edytora tekstu
+
+# Jeśli są konflikty, rozwiązujemy je automatycznie i kontynuujemy
+git add .
+git commit -m "Rozwiązano konflikty podczas merge"
+
+# Pushuj zmiany
 git push https://github.com/Hicior/Calendesk.git main
