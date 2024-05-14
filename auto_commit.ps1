2d93b35fafb303994ddf8d290829449876094352
Wojtek <w.kuczkowski@mentzen.pl>
2024-05-14 10:38:56 +0200

    2

---
+++ auto_commit.ps1
@@ -0,0 +1,11 @@
+# Navigate to the repository directory
+cd "C:\Users\Nagrywanie\OneDrive - Sławomir Mentzen\Pulpit\Calendesk-HTML-CSS"
+
+# Add all changes to git
+git add .
+
+# Commit changes with a message
+git commit -m "2"
+
+# Push changes to the remote repository
+git push https://github.com/Hicior/Calendesk.git main
